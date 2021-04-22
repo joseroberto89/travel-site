e2c2b349738339649a16d3f02ab783dc6621ea67
Jose <joseroberto2989@gmail.com>
2021-04-22 19:40:17 +0100

    AWS lambda

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "welcome to the Super Secret Area"
-    })
+    const secretContent = `
+    <h3>Welcome to the Secret Area</h3>
+    <p>Here we can tell you the sky is <strong>blue</strong></p>`
+
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,   //status code to sucess
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401   //status code to Fail
+        })
+    }
+    
+    
 }
