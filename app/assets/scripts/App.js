471f1e5636afc7df7674be650392036e5e423d73
Jose <joseroberto2989@gmail.com>
2021-04-22 21:55:08 +0100

    just a intro do react

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,6 +6,17 @@ import StickyHeader from "./modules/StickyHeader"
 import ClientArea from "./modules/ClientArea"
 
 
+// React Related Code
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+// Import React Components
+import MyAmazingComponent from "./modules/MyAmazingComponent"
+
+ReactDOM.render(<MyAmazingComponent />, document.querySelector("#my-react-example"))
+
+
 new ClientArea()
 new StickyHeader
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
